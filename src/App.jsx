6bf9671b9e628c8c66a1ec51cb1e5512d0810cbd
karn30chavda <karanchavda543@gmail.com>
2025-07-19 15:31:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="text-3xl font-bold rounded-xl text-center m-4 p-4 bg-gray-600 text-white">
+        Welcome to React Mega Project
+      </div>
+    </>
+  );
+}
+
+export default App;
