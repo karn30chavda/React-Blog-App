dec12c80f5e8a03a7114c2cba2e23cc96905939d
karn30chavda <karanchavda543@gmail.com>
2025-07-28 23:51:52 +0530

    navbar error fix

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,8 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { Protected as AuthLayout, Login } from "./components/index.js";
 import "./index.css";
-import {
-  default as AllPost,
-  default as CreatePost,
-} from "./pages/CreatePost.jsx";
+import AllPosts from "./pages/AllPost.jsx";
+import CreatePost from "./pages/CreatePost.jsx";
 import EditPost from "./pages/EditPost";
 import Home from "./pages/Home.jsx";
 import Post from "./pages/Post";
@@ -43,7 +41,7 @@ const router = createBrowserRouter([
         path: "/all-posts",
         element: (
           <AuthLayout authentication>
-            <AllPost />
+            <AllPosts />
           </AuthLayout>
         ),
       },
